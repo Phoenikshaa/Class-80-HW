4b2413ce0e57e3a71aa05b80f3d5d5f7489246e2
Phoenikshaa <77309769+Phoenikshaa@users.noreply.github.com>
2021-05-19 17:58:04 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,72 +1,46 @@
-names_of_people = [];
-
-function Search()
-{
-    var s = document.getElementById("s1").value;
-    var found = 0;
-    var j;
-    for(j=0; j<names_of_people_.length; j++)
-    {
-        if(s == names_of_people[j]){
-            found = found + 1;
-        }
-    }
-    document.getElementById("p2").innerHTML = "name found" + found + "time/s";
-    console.log ("found name" + found + "time/s");
-}
-
+var names_of_people = [];
+    
 function submit()
 {
+    var GuestName = document.getElementById("name1").value;
+	names_of_people.push(GuestName);
 
-    var GuestName = document.getElementById("Enter the Guest Input Box").value;
-    console.log(GuestName)
-    names_of_people.push(GuestName);
-    document.getElementById("display_name").innerHTML = names_of_people
-    
-}
+	console.log(GuestName);
+        
+    console.log(names_of_people);
+    var lenght_of_name = names_of_people.length;
+    console.log(lenght_of_name);
+	document.getElementById("display_name").innerHTML=names_of_people.toString();
+	
+   }
 
-function sort()
+function sorting()
 {
-    var GuestName = document.getElementById("Enter the Guest Input Box").value;
-    console.log(GuestName)
-    names_of_people.push(GuestName);
-    document.getElementById("display_name").innerHTML = names_of_people
-
-
-
-
-
-    names_of_people.sort();
-    console.log(names_of_people);
-
-    var display_name_for_sorting = [];
-
-    var display_name_for_sorting = display_name_for_sorting;
-    console.log(names_of_people);
-
-    for (var k = 0; k < display_name_for_sorting; k++)
-    {
-        display_name_for_sorting.push("<display_name_for_sorting>NAME -" + name_of_the_student_array[k] + "</display_name_for_sorting>");
-        console.log(display_name_for_sorting);
-    }
-
-    remove_commas.sort();
-
-    var remove_commas = display_name_for_sorting.join(" ");
-    console.log(remove_commas);
-
-    document.getElementById("display_name_for_sorting_without_commas").innerHTML = remove_commas;
-}
-    
+	names_of_people.sort();
+	var i= names_of_people.join("<br>");
+    console.log(names_of_people);		
+	document.getElementById("sorted").innerHTML=i.toString();
+	}
 
 function show()
 {
-    var GuestName = document.getElementById("Enter the Guest Input Box").value;
-    console.log(GuestName)
-    names_of_people.push(GuestName);
-    document.getElementById("display_name_for_show").innerHTML = names_of_people
-    <br>
+	var i= names_of_people.join("<br>");
+	console.log(names_of_people);
+	document.getElementById("p1").innerHTML=i.toString();
+	document.getElementById("sort_button").style.display="block";
+	}
 
-
+function searching()
+{
+	var s= document.getElementById("s1").value;
+	var found=0;
+	var j;
+	for(j=0; j<names_of_people.length; j++)
+		{
+			if(s==names_of_people[j]){
+				found=found+1;
+			}	
+		}
+	document.getElementById("p2").innerHTML="name found "+found+" time/s";
+	console.log("found name "+found+" time/s");
 }
-
